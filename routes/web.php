6708db82e97da1fa203635d8cee8ae042874c200
tungnd194713 <tung-nguyen@dimage.co.jp>
2023-01-16 17:40:52 +0700

    add user profile + model binding

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,13 +21,16 @@ Route::get('/register', [App\Http\Controllers\AuthController::class, 'showRegist
 Route::post('/register', [App\Http\Controllers\AuthController::class, 'register'])->name('register');
 Route::post('/logout', [App\Http\Controllers\AuthController::class, 'logout'])->name('logout');
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/profile/{user?}', function (User $user) {
+    return view('profile', compact('user'));
+})->name('profile');
 
 Route::group(['middleware' => ['auth', 'web']], function () {
     Route::get('/timesheet', [App\Http\Controllers\TimesheetController::class, 'index'])->name('timsheet.index');
     Route::get('/timesheet/{id}', [App\Http\Controllers\TimesheetController::class, 'show'])->name('timsheet.show');
     Route::get('/timesheet-detail', [App\Http\Controllers\TimesheetController::class, 'showByDate'])->name('timsheet.show.by.date');
     Route::post('/timesheet', [App\Http\Controllers\TimesheetController::class, 'store'])->name('timsheet.create');
-    Route::put('/timesheet/{id}', [App\Http\Controllers\TimesheetController::class, 'update'])->name('timesheet.update');
+    // Route::put('/timesheet/{id}', [App\Http\Controllers\TimesheetController::class, 'update'])->name('timesheet.update');
     Route::put('/check-in', [App\Http\Controllers\TimesheetController::class, 'checkIn'])->name('user.check.in');
     Route::put('/check-out', [App\Http\Controllers\TimesheetController::class, 'checkOut'])->name('user.check.out');
 });
